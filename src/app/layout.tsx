b7b93f2a5f9d826fd52901c92dae046968f08d10
Yejin <qja086351@naver.com>
2026-05-16 12:12:28 +0900

    feat(analytics): Meta Pixel 설치
    - layout.tsx에 next/script afterInteractive로 fbevents.js 로드
    - NEXT_PUBLIC_META_PIXEL_ID 환경변수로 Pixel ID 관리
    - ID 미설정 시 스크립트 비활성화 (조건부 렌더링)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider';
 const THEME_FLICKER_SCRIPT = `(function(){try{var s=localStorage.getItem('winemine.theme');if(s==='light'||s==='dark'){document.documentElement.setAttribute('data-theme',s);}}catch(e){}})();`;
 
 const GA_ID = 'G-7V8ZDT0TYX';
+const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
 const playfairDisplay = Playfair_Display({
   subsets: ['latin'],
@@ -87,6 +88,24 @@ export default async function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        {META_PIXEL_ID && (
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${META_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
